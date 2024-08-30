72347c6966faacb3046dc0d26c063fcc94d374c2
kubbal <kublerbalazs@gmail.com>
2024-08-30 12:00:07 +0200

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { GameComponent } from './component/game/game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: 'game', component: GameComponent },
+];
