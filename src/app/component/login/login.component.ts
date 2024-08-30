e9fdeb52304f1607364e3b2a163c3d8b8a942113
kubbal <kublerbalazs@gmail.com>
2024-08-30 11:59:19 +0200

    login component

---
+++ src/app/component/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserInterface } from '../../model/user.interface';
+import { Store } from '@ngrx/store';
+import { selectLoginError, selectUsers } from '../../store/selector/user.selector';
+import { loadUsers, loginUser } from '../../store/action/user.action';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  users$: Observable<UserInterface[]>;
+  error$: Observable<string | null>;
+  selectedUserId: string = '';
+  password: string = '';
+
+  constructor(private store: Store) {
+    this.users$ = this.store.select(selectUsers);
+    this.error$ = this.store.select(selectLoginError);
+    this.store.dispatch(loadUsers());
+  }
+
+  onUserSelect(event: any) {
+    this.selectedUserId = event.target.value;
+  }
+
+  login() {
+    this.store.dispatch(loginUser({ userId: this.selectedUserId, password: this.password }));
+  }
+}
