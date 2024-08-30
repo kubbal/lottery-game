7d8264a24a063ccdf3f3d568cc032113bf5c8c2a
kubbal <kublerbalazs@gmail.com>
2024-08-30 11:58:03 +0200

    add userSelector

---
+++ src/app/store/selector/user.selector.ts
@@ -0,0 +1,8 @@
+import { createSelector, createFeatureSelector } from '@ngrx/store';
+import { UserState } from '../reducer/user.reducer';
+
+export const selectUserState = createFeatureSelector<UserState>('user');
+
+export const selectUsers = createSelector(selectUserState, (state: UserState) => state.users);
+export const selectLoggedInUser = createSelector(selectUserState, (state: UserState) => state.loggedInUser);
+export const selectLoginError = createSelector(selectUserState, (state: UserState) => state.error);
