dd9c87ef25ede549cfe92beb8dcc1d993bcee299
kubbal <kublerbalazs@gmail.com>
2024-09-01 13:22:22 +0200

    authGuard using ngrx store

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,10 +1,20 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../service/auth.service';
+import { map, take } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  if (inject(AuthService).isAuthenticated())
-    return true;
-  inject(Router).navigate(['']);
-  return false;
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.isAuthenticated().pipe(
+    take(1),
+    map(isAuthenticated => {
+      if (isAuthenticated) {
+        return true;
+      }
+      router.navigate(['']);
+      return false;
+    })
+  )
 };
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,8 @@
 import { inject, Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { loginUser } from '../store/action/user.action';
+import { selectLoggedInUser } from '../store/selector/user.selector';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +10,12 @@ import { loginUser } from '../store/action/user.action';
 export class AuthService {
 
   store = inject(Store);
-  private isLoggedIn = false;
 
   login(userId: string, password: string) {
     this.store.dispatch(loginUser({ userId, password }));
-    this.isLoggedIn = true;
   }
 
-  isAuthenticated(): boolean {
-    return this.isLoggedIn;
+  isAuthenticated(): Observable<boolean> {
+    return this.store.select(selectLoggedInUser).pipe(map(user => !!user));
   }
 }
